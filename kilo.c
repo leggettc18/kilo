8b13b515859e47b3512c5117c90e3dea883c0168
Christopher Leggett <chris@leggett.dev>
2021-07-10 22:52:44 -0400

    Disables Ctrl+S/Q

--- kilo.c
+++ kilo.c
@@ -37,6 +37,7 @@ void enableRawMode() {
 
     // sets necessary flags to enable raw mode.
     struct termios raw = orig_termios;
+    raw.c_iflag &= ~(IXON);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 
