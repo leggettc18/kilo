1fde9c7cea039b51c2be58f245cdcb462a0df22b
Christopher Leggett <chris@leggett.dev>
2021-07-11 13:40:04 -0400

    Adds horizontal scrolling.

--- kilo.c
+++ kilo.c
@@ -54,6 +54,7 @@ struct editorConfig
 {
     int cx, cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -344,10 +345,7 @@ void editorMoveCursor(int key)
         }
         break;
     case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1)
-        {
-            E.cx++;
-        }
+        E.cx++;
         break;
     case ARROW_UP:
         if (E.cy != 0)
@@ -456,12 +454,23 @@ void abFree(struct abuf *ab)
  * Checks for if the cursor is off the screen and sets the row
  * offset accordingly.
 */
-void editorScroll() {
-    if (E.cy < E.rowoff) {
+void editorScroll()
+{
+    if (E.cy < E.rowoff)
+    {
         E.rowoff = E.cy;
     }
-    if (E.cy >= E.rowoff + E.screenrows) {
+    if (E.cy >= E.rowoff + E.screenrows)
+    {
         E.rowoff = E.cy - E.screenrows + 1;
+    }
+    if (E.cx < E.coloff)
+    {
+        E.coloff = E.cx;
+    }
+    if (E.cx >= E.coloff + E.screencols)
+    {
+        E.coloff = E.cx - E.screencols + 1;
     }
 }
 
@@ -502,10 +511,12 @@ void editorDrawRows(struct abuf *ab)
         }
         else
         {
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0)
+                len = 0;
             if (len > E.screencols)
                 len = E.screencols;
-            abAppend(ab, E.row[filerow].chars, len);
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
         abAppend(ab, "\x1b[K", 3);
 
@@ -532,7 +543,8 @@ void editorRefreshScreen()
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+             (E.cx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -553,6 +565,7 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
