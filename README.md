da61f3f55401cd8385af8c93d0299eb096b1282c
Christopher Leggett <chris@leggett.dev>
2021-07-11 23:31:56 -0400

    Implements a Save As feature.

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ supplying the filename as an argument, you can navigate with the arrow keys,
 insert text by typing, backspace and delete to remove characters, and insert
 new lines with enter. And you can save your work with Ctrl + S. It is very basic,
 there is no auto-indent or syntax highlighting or anything of the sort, but it
-does function.
+does function. You can also open up the editor with no filename argument to 
+create a new file and pick a filename on the first save.
 ## Instructions
 If you wish to build it for yourself, just install clang and make, clone the
 repo, `cd` into it, and run `make` It will output an executable file called
--- kilo.c
+++ kilo.c
@@ -81,6 +81,8 @@ struct editorConfig E;
 /*** prototypes ***/
 
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 /*** terminal ***/
 
@@ -590,8 +592,13 @@ void editorOpen(char *filename)
 */
 void editorSave()
 {
-    if (E.filename == NULL)
-        return;
+    if (E.filename == NULL){
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        if (E.filename == NULL) {
+            editorSetStatusMessage("");
+            return;
+        }
+    }
 
     int len;
     char *buf = editorRowsToString(&len);
@@ -617,6 +624,53 @@ void editorSave()
 }
 
 /*** input ***/
+
+/* Function editorPrompt
+ * ---------------------------------------------------------------
+ * Prompts the user for input and returns it to the caller
+ * when "Enter" is pressed.
+ * 
+ * prompt: A string containing info on what the user is being
+ * prompted for. It is expected to contain "%s" somewhere in the
+ * string, as that is what will display the user's input.
+ * 
+ * returns: a string containing the user's input.
+*/
+char *editorPrompt(char *prompt) {
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1) {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE) {
+            if (buflen != 0) buf[--buflen] = '\0';
+        }
+        else if (c == '\x1b') {
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        }
+        else if (c == '\r') {
+            if (buflen != 0) {
+                editorSetStatusMessage("");
+                return buf;
+            }
+        } else if (!iscntrl(c) && c < 128) {
+            if (buflen == bufsize - 1) {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
 
 /* Function: editorMoveCursor
  * -----------------------------------------------------
