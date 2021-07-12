011dc9c0b6f63afd04b64baf7dcebc487425e84c
Christopher Leggett <chris@leggett.dev>
2021-07-11 22:06:34 -0400

    Implements confirm quit dialog when unsaved changes are present.

--- kilo.c
+++ kilo.c
@@ -32,6 +32,7 @@
 #define CTRL_KEY(k) ((k)&0x1f)
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 enum editorKey
 {
@@ -564,6 +565,8 @@ void editorMoveCursor(int key)
 */
 void editorProcessKeypress()
 {
+    static int quit_times = KILO_QUIT_TIMES;
+
     int c = editorReadKey();
 
     switch (c)
@@ -572,6 +575,12 @@ void editorProcessKeypress()
         /*TODO*/
         break;
     case CTRL_KEY('q'):
+        if (E.dirty && quit_times > 0) {
+            editorSetStatusMessage("WARNING!! File has unsaved changes. "
+                "Press Ctrl-Q %d more times to quit.", quit_times);
+            quit_times--;
+            return;
+        }
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
@@ -625,6 +634,7 @@ void editorProcessKeypress()
         editorInsertChar(c);
         break;
     }
+    quit_times = KILO_QUIT_TIMES;
 }
 
 /*** append buffer ***/
